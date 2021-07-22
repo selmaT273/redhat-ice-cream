e738d8e6333489a937464aa12101189412b34f80
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-21 19:19:57 -0500

    removed part of reverse logic example that accidentally got left in last commit

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ let flavorOfTheDay = 'unicorn';
 
 let menu = 'chocolate, rocky road, mint chip, superman, batman';
 
-// document.write('Today\'s menu is: ' + flavorOfTheDay + ', ' + menu);
-
 let hasSprinkles = false;
 let medium = 3;
 
@@ -12,10 +10,9 @@ let myItems = prompt('what would you like to order?');
 
 myOrder = myItems;
 
-
 let wantsSprinkles = prompt('would you like sprinkles on top?');
 
-if(wantsSprinkles !== 'yes') {
+if(wantsSprinkles === 'yes') {
   myOrder = myItems + ' and sprinkles on top!';
 } else if (wantsSprinkles === 'no') {
   myOrder = myItems + ' without sprinkles on top';
@@ -25,6 +22,7 @@ if(wantsSprinkles !== 'yes') {
 
 document.write('You want to order:' + myOrder);
 
+// document.write('Today\'s menu is: ' + flavorOfTheDay + ', ' + menu);
 
 // alert('Welcome! Today\'s flavor of the day is:' + ' ' + flavorOfTheDay);
 
@@ -33,3 +31,7 @@ document.write('You want to order:' + myOrder);
 
 // document.write(userName);
 
+
+// let userChoice = prompt('do you want your phone to be...');
+
+// alert(userChoice);
