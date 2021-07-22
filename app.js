fcdfa6eb8be1cc4bb25da71e729b11c99bdaa95c
Stacey Teltser <stacey.teltser@gmail.com>
2021-07-22 11:43:25 -0500

    refactoredto demo function

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-let flavorOfTheDay = 'unicorn';
+let mySize = prompt('what size would you like?');
+let myFlavor = prompt('what flavor would you like?');
+let myToppings = prompt('what toppings?');
 
-let menu = 'chocolate, rocky road, mint chip, superman, batman';
 
-let hasSprinkles = false;
-let medium = 3;
-
-let myOrder = '';
-let myItems = prompt('what would you like to order?');
-
-myOrder = myItems;
-
-let wantsSprinkles = prompt('would you like sprinkles on top?');
-
-if(wantsSprinkles === 'yes') {
-  myOrder = myItems + ' and sprinkles on top!';
-} else if (wantsSprinkles === 'no') {
-  myOrder = myItems + ' without sprinkles on top';
-} else {
-  myOrder = myItems;
+function makeMyOrder(size, flavor, toppings){
+   document.write('here is your order ' + size + ' ' + flavor + ' ' + toppings);
 }
 
-document.write('You want to order:' + myOrder);
+makeMyOrder(mySize, myFlavor, myToppings);
+
+
+
+
+
+// let flavorOfTheDay = 'unicorn';
+
+// let menu = 'chocolate, rocky road, mint chip, superman, batman';
+
+// let myItems = prompt('what would you like to order?');
+
+// myOrder = myItems;
+
+// let wantsSprinkles = prompt('would you like sprinkles on top?');
+
+// let wantsSprinklesLowercase = wantsSprinkles.toLowerCase();
+
+// if(wantsSprinklesLowercase === 'yes') {
+//   myOrder = myItems + ' and sprinkles on top!';
+// } else if (wantsSprinklesLowercase === 'no') {
+//   myOrder = myItems + ' without sprinkles on top';
+// } else {
+//   myOrder = myItems;
+// }
+
+
+
+
+
+// document.write('You want to order:' + myOrder);
 
 // document.write('Today\'s menu is: ' + flavorOfTheDay + ', ' + menu);
 
